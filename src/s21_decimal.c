d9c8285ea2c835d93b9a3aceb9af6e3f53ab1de8
scrimgeo <scrimgeo@student.21-school.ru>
2023-02-24 11:41:16 +0700

    scrimgeo: all function write

---
+++ src/s21_decimal.c
@@ -0,0 +1,292 @@
+#include "s21_decimal.h"
+
+#include <math.h>
+
+#include "utils.h"
+
+int s21_negate(s21_decimal value, s21_decimal *result) {
+  int error = (result) ? 0 : 1;
+  if (!error) {
+    for (uint16_t i = 0; i < 3; ++i) result->bits[i] = value.bits[i];
+    result->bits[3] = value.bits[3] ^ MINUS;
+  }
+  return error;
+}
+
+int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+  int overflow = (result) ? 0 : 4;
+  if ((iszero(value_1) || iszero(value_2)) && !overflow)
+    *result = (iszero(value_1)) ? value_2 : value_1;
+  else if (!overflow) {
+    int sign_1 = isminus(value_1), sign_2 = isminus(value_2);
+    work_decimal v_1 = convert2work(value_1), v_2 = convert2work(value_2),
+                 res = initwork();
+    overflow = pointequalize(&v_1, &v_2);
+    if (sign_1 == sign_2)
+      res = addbits(v_1, v_2);
+    else {
+      int who_grate = compearbits(v_1, v_2);
+      if (who_grate == 1)
+        res = subbits(v_1, v_2);
+      else if (who_grate == 1) {
+        sign_1 = sign_2;
+        res = subbits(v_2, v_1);
+      }
+    }
+    overflow = normalize(&res);
+    if (overflow)
+      overflow = (sign_1) ? 2 : 1;
+    else
+      *result = convert2s21(res, sign_1);
+  }
+  return overflow;
+}
+
+int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+  s21_decimal not_value_2 = set21(0, 0, 0, 0);
+  s21_negate(value_2, &not_value_2);
+  return s21_add(value_1, not_value_2, result);
+}
+
+int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+  int overflow = (result) ? 0 : 4;
+  if ((iszero(value_1) || iszero(value_2)) && !overflow)
+    *result = set21(0, 0, 0, 0);
+  else if (!overflow) {
+    int sign = (isminus(value_1) == isminus(value_2)) ? 0 : 1;
+    work_decimal v_1 = convert2work(value_1), v_2 = convert2work(value_2),
+                 res = initwork();
+    res.exp = v_1.exp + v_2.exp;
+    sumworkbits(&res, 0, v_1.bits[0] * v_2.bits[0], 0, 0);
+    sumworkbits(&res, 1, v_1.bits[1] * v_2.bits[0], v_1.bits[0] * v_2.bits[1],
+                0);
+    sumworkbits(&res, 2, v_1.bits[0] * v_2.bits[2], v_1.bits[1] * v_2.bits[1],
+                v_1.bits[2] * v_2.bits[0]);
+    sumworkbits(&res, 3, v_1.bits[1] * v_2.bits[2], v_1.bits[2] * v_2.bits[1],
+                0);
+    sumworkbits(&res, 4, v_1.bits[2] * v_2.bits[2], 0, 0);
+    overflow = normalize(&res);
+    if (overflow)
+      overflow = (sign) ? 2 : 1;
+    else
+      *result = convert2s21(res, sign);
+  }
+  return overflow;
+}
+
+int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+  int overflow = (result) ? 0 : 4;
+  int sign = (isminus(value_1) == isminus(value_2)) ? 0 : 1;
+  if (iszero(value_2))
+    overflow = 3;
+  else if (iszero(value_1) && !overflow)
+    *result = set21(0, 0, 0, 0);
+  else if (!overflow) {
+    work_decimal v_1 = convert2work(value_1), v_2 = convert2work(value_2),
+                 res = initwork();
+    if (v_1.exp < v_2.exp) {
+      for (uint16_t i = 0; i < v_2.exp - v_1.exp; ++i) bits10up(&v_1);
+    } else {
+      for (uint16_t i = 0; i < v_1.exp - v_2.exp; ++i) bits10up(&v_2);
+    }
+    if (compearbits(v_1, shiftleft(v_2, 96)) < 0) {
+      v_1 = divmain(v_1, v_2, &res);
+      if (!iszerow(v_1)) divtail(v_1, v_2, &res);
+    } else
+      overflow = (sign) ? 2 : 1;
+    if (overflow == 0) {
+      overflow = normalize(&res);
+      *result = convert2s21(res, sign);
+    }
+  }
+  return overflow;
+}
+
+int s21_mod(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+  int overflow = (result) ? 0 : 4;
+  if (iszero(value_2))
+    overflow = 3;
+  else if (!overflow) {
+    int sign = isminus(value_1);
+    if (s21_is_less(value_1, value_2))
+      *result = value_1;
+    else {
+      work_decimal v_1 = convert2work(value_1), v_2 = convert2work(value_2),
+                   res = initwork();
+      if (v_1.exp < v_2.exp) {
+        for (uint16_t i = 0; i < v_2.exp - v_1.exp; ++i) bits10up(&v_1);
+      } else {
+        for (uint16_t i = 0; i < v_1.exp - v_2.exp; ++i) bits10up(&v_2);
+      }
+      v_1.exp = v_2.exp;
+      if (compearbits(v_1, shiftleft(v_2, 96)) < 0) res = divremain(v_1, v_2);
+      overflow = normalize(&res);
+      *result = convert2s21(res, sign);
+    }
+  }
+  return overflow;
+}
+
+int s21_is_less(s21_decimal value_1, s21_decimal value_2) {
+  int res = 0;
+  if (iszero(value_1)) {
+    if (!iszero(value_2)) res = (isminus(value_2)) ? 0 : 1;
+  } else if (iszero(value_2))
+    res = (isminus(value_1)) ? 1 : 0;
+  else {
+    int sign_1 = isminus(value_1), sign_2 = isminus(value_2);
+    if (sign_1 != sign_2)
+      res = (sign_1 > sign_2) ? 1 : 0;
+    else {
+      work_decimal v_1 = convert2work(value_1), v_2 = convert2work(value_2);
+      pointequalize(&v_1, &v_2);
+      int who_grate = compearbits(v_1, v_2);
+      if (who_grate == 1)
+        res = (sign_1) ? 1 : 0;
+      else if (who_grate == -1)
+        res = (sign_1) ? 0 : 1;
+    }
+  }
+  return res;
+}
+
+int s21_is_less_or_equal(s21_decimal value_1, s21_decimal value_2) {
+  return (s21_is_equal(value_1, value_2)) ? 1 : s21_is_less(value_1, value_2);
+}
+
+int s21_is_greater(s21_decimal value_1, s21_decimal value_2) {
+  return (s21_is_less_or_equal(value_1, value_2)) ? 0 : 1;
+}
+
+int s21_is_greater_or_equal(s21_decimal value_1, s21_decimal value_2) {
+  return (s21_is_less(value_1, value_2)) ? 0 : 1;
+}
+
+int s21_is_equal(s21_decimal value_1, s21_decimal value_2) {
+  int res = 0;
+  if (iszero(value_1))
+    res = (iszero(value_2)) ? 1 : 0;
+  else if (isminus(value_1) == isminus(value_2)) {
+    work_decimal v_1 = convert2work(value_1), v_2 = convert2work(value_2);
+    pointequalize(&v_1, &v_2);
+    if (compearbits(v_1, v_2) == 0) res = 1;
+  }
+  return res;
+}
+
+int s21_is_not_equal(s21_decimal value_1, s21_decimal value_2) {
+  return (s21_is_equal(value_1, value_2)) ? 0 : 1;
+}
+
+int s21_from_int_to_decimal(int src, s21_decimal *dst) {
+  int error = (dst) ? 0 : 1;
+  if (!error) *dst = (src > 0) ? set21(0, 0, 0, src) : set21(MINUS, 0, 0, -src);
+  return error;
+}
+
+int s21_from_decimal_to_int(s21_decimal src, int *dst) {
+  int overflow = (dst) ? 0 : 1;
+  if (!overflow) {
+    s21_decimal tranc_src = set21(0, 0, 0, 0);
+    s21_truncate(src, &tranc_src);
+    overflow =
+        (tranc_src.bits[2] || tranc_src.bits[1] || tranc_src.bits[0] > NOTMINUS)
+            ? 1
+            : 0;
+    if (!overflow)
+      *dst = (isminus(tranc_src)) ? -tranc_src.bits[0] : tranc_src.bits[0];
+  }
+  return overflow;
+}
+
+int s21_from_float_to_decimal(float src, s21_decimal *dst) {
+  int overflow = (dst) ? 0 : 1;
+  if (isnan(src)) overflow = 1;
+  if (!overflow) *dst = set21(0, 0, 0, 0);
+  if (src && !overflow) {
+    int sign = (src < 0) ? 1 : 0;
+    src = fabs(src);
+    if (src < 1e-28 || src > 7.92281625e28)
+      overflow = 1;
+    else {
+      char foo[15];
+      sprintf(foo, "%+.6e", src);
+      int val = char2num(foo[1]);
+      for (uint16_t i = 3; i < 9; ++i) val = 10 * val + char2num(foo[i]);
+      int exp = 0;
+      sscanf(foo + 10, "%d", &exp);
+      exp -= 6;
+      work_decimal value = initwork();
+      value.bits[0] = val;
+      if (exp > 0) {
+        for (uint16_t i = 0; i < exp; ++i) overflow = bits10up(&value);
+      } else {
+        value.exp = -exp;
+        overflow = normalize(&value);
+      }
+      *dst = convert2s21(value, sign);
+    }
+  }
+  return overflow;
+}
+
+int s21_from_decimal_to_float(s21_decimal src, float *dst) {
+  int error = (dst) ? 0 : 1;
+  if (iszero(src) && error == 0)
+    *dst = 0;
+  else if (!error) {
+    int sign = (isminus(src)) ? -1 : 1;
+    int exp = (src.bits[3] & NOTMINUS) >> 16;
+    uint32_t value[3];
+    for (uint16_t i = 0; i < 3; ++i) value[i] = (unsigned int)src.bits[i];
+    *dst = (pow(2, 64) * value[2] + pow(2, 32) * value[1] + value[0]) *
+           pow(10, -exp) * sign;
+  }
+  return error;
+}
+
+int s21_floor(s21_decimal value, s21_decimal *result) {
+  int error = (result) ? 0 : 1;
+  if (iszero(value) && error == 0)
+    *result = set21(0, 0, 0, 0);
+  else if (!error) {
+    int sign = isminus(value), notint = 0;
+    work_decimal work_value = convert2work(value);
+    while (work_value.exp) {
+      int remainder = dellast(&work_value);
+      notint = (notint) ? notint : remainder;
+    }
+    if (notint && sign) addnum(&work_value, 1);
+    if (work_value.bits[3]) error = 1;
+    if (!error) *result = convert2s21(work_value, sign);
+  }
+  return error;
+}
+
+int s21_round(s21_decimal value, s21_decimal *result) {
+  int error = (result) ? 0 : 1;
+  if (iszero(value) && error == 0)
+    *result = set21(0, 0, 0, 0);
+  else if (!error) {
+    int sign = isminus(value), remainder = 0;
+    work_decimal work_value = convert2work(value);
+    while (work_value.exp) remainder = dellast(&work_value);
+    if (remainder > 4) addnum(&work_value, 1);
+    if (work_value.bits[3]) error = 1;
+    if (!error) *result = convert2s21(work_value, sign);
+  }
+  return error;
+}
+
+int s21_truncate(s21_decimal value, s21_decimal *result) {
+  int error = (result) ? 0 : 1;
+  if (iszero(value) && error == 0)
+    *result = set21(0, 0, 0, 0);
+  else if (!error) {
+    int sign = isminus(value);
+    work_decimal work_value = convert2work(value);
+    while (work_value.exp) dellast(&work_value);
+    *result = convert2s21(work_value, sign);
+  }
+  return error;
+}
---
+++ src/utils.c
@@ -0,0 +1,303 @@
+#include "utils.h"
+
+#include <stdio.h>
+
+int iszero(s21_decimal value) {
+  return value.bits[0] == 0 && value.bits[1] == 0 && value.bits[2] == 0;
+}
+
+int iszerow(work_decimal value) {
+  int res = 1;
+  for (uint16_t i = 0; i < WORKBITS; ++i) res &= (value.bits[i] == 0);
+  return res;
+}
+
+int isminus(s21_decimal value) { return (value.bits[3] & MINUS) == MINUS; }
+
+void setminus(s21_decimal *value) { value->bits[3] |= MINUS; }
+
+work_decimal convert2work(s21_decimal value) {
+  work_decimal result;
+  for (uint16_t i = 0; i < 3; ++i)
+    result.bits[i] = bitwithoutover(value.bits[i]);
+  for (uint16_t i = 3; i < WORKBITS; ++i) result.bits[i] = 0;
+  result.exp = (value.bits[3] & NOTMINUS) >> 16;
+  return result;
+}
+
+s21_decimal convert2s21(work_decimal value, int sign) {
+  s21_decimal result;
+  for (uint16_t i = 0; i < 3; ++i)
+    result.bits[i] = bitwithoutover(value.bits[i]);
+  result.bits[3] = (value.exp << 16) | ((sign) ? MINUS : 0);
+  return result;
+}
+
+uint64_t bitwithoutover(uint64_t bit) { return bit & MAX4BIT; }
+
+uint64_t getoverflow(uint64_t bit) { return bit >> 32; }
+
+uint64_t addolderbit(uint64_t smallerbit, uint64_t olderbit) {
+  return smallerbit + (olderbit << 32);
+}
+
+unsigned int bits10up(work_decimal *value) {
+  unsigned int overflow = 0;
+  work_decimal foo = *value;
+  for (uint16_t i = 0; i < WORKBITS; ++i) {
+    value->bits[i] = bitwithoutover(10 * foo.bits[i] + overflow);
+    overflow = getoverflow(10 * foo.bits[i] + overflow);
+  }
+  if (overflow) *value = foo;
+  return overflow;
+}
+
+unsigned int bits10down(work_decimal *value) {
+  uint64_t remainder = 0;
+  work_decimal foo = *value;
+  for (int16_t i = WORKBITS - 1; i >= 0; --i) {
+    value->bits[i] = addolderbit(foo.bits[i], remainder) / 10;
+    remainder = addolderbit(foo.bits[i], remainder) % 10;
+  }
+  return (unsigned int)remainder;
+}
+
+unsigned int addlast(work_decimal *value) {
+  unsigned int overflow = 0;
+  if (bits10up(value))
+    overflow = 1;
+  else
+    value->exp += 1;
+  return overflow;
+}
+
+unsigned int dellast(work_decimal *value) {
+  value->exp -= 1;
+  return bits10down(value);
+}
+
+unsigned int pointequalize(work_decimal *value1, work_decimal *value2) {
+  unsigned int overflow = 0;
+  if (value1->exp != value2->exp) {
+    int whoismax = (value1->exp > value2->exp) ? 1 : 2;
+    work_decimal *max = (whoismax == 1) ? value1 : value2;
+    work_decimal *min = (whoismax == 2) ? value1 : value2;
+    int16_t expdif = max->exp - min->exp;
+    for (; expdif > 0 && overflow == 0; --expdif) overflow = addlast(min);
+  }
+  return overflow;
+}
+
+int bankround(work_decimal value, unsigned int remander,
+              unsigned int countround) {
+  int roundup = 0;
+  if (remander > 5)
+    roundup = 1;
+  else if (remander == 5) {
+    if (countround > 1)
+      roundup = 1;
+    else if (value.bits[0] & 1)
+      roundup = 1;
+  }
+  return roundup;
+}
+
+int needdown(work_decimal value) {
+  int need = 0;
+  for (uint16_t i = 3; i < WORKBITS; ++i) need |= (value.bits[i] != 0);
+  if (need == 0) need = value.exp > MAXEXP;
+  return need;
+}
+
+int normalize(work_decimal *value) {
+  int overflow = 0;
+  unsigned int remainder = 0, countround = 0;
+  for (int16_t i = 0; i < WORKBITS - 1; ++i) {
+    value->bits[i + 1] += getoverflow(value->bits[i]);
+    value->bits[i] = bitwithoutover(value->bits[i]);
+  }
+  while (needdown(*value) && value->exp > 0) {
+    remainder = dellast(value);
+    if (remainder) countround++;
+  }
+  overflow = needdown(*value);
+  if (bankround(*value, remainder, countround) && overflow == 0) {
+    if (addnum(value, 1)) overflow = 1;
+    if (needdown(*value)) {
+      if (value->exp > 0) {
+        dellast(value);
+        addnum(value, 1);
+      } else
+        overflow = 1;
+    }
+  }
+  if (value->exp > 0 && overflow == 0) {
+    work_decimal foo = *value;
+    int doing = 1;
+    remainder = 0;
+    while (doing) {
+      if (value->exp > 0) {
+        remainder = dellast(&foo);
+        if (remainder == 0)
+          *value = foo;
+        else
+          doing = 0;
+      } else
+        doing = 0;
+    }
+  }
+  return overflow;
+}
+
+int compearbits(work_decimal value_1, work_decimal value_2) {
+  int compear = 0;
+  for (int16_t i = WORKBITS - 1; i >= 0 && compear == 0; --i) {
+    if (value_1.bits[i] != value_2.bits[i])
+      compear = (value_1.bits[i] > value_2.bits[i]) ? 1 : -1;
+  }
+  return compear;
+}
+
+unsigned int addnum(work_decimal *value, uint16_t num) {
+  unsigned int overflow = num;
+  work_decimal foo = *value;
+  for (uint16_t i = 0; i < WORKBITS; ++i) {
+    foo.bits[i] += overflow;
+    overflow = getoverflow(foo.bits[i]);
+    foo.bits[i] = bitwithoutover(foo.bits[i]);
+  }
+  if (overflow == 0) *value = foo;
+  return overflow;
+}
+
+work_decimal addbits(work_decimal value_1, work_decimal value_2) {
+  unsigned int overflow = 0;
+  work_decimal res = value_1;
+  for (uint16_t i = 0; i < WORKBITS; ++i) {
+    res.bits[i] += value_2.bits[i] + overflow;
+    overflow = getoverflow(res.bits[i]);
+    res.bits[i] = bitwithoutover(res.bits[i]);
+  }
+  return res;
+}
+
+void sumworkbits(work_decimal *value, uint32_t index, uint64_t add1,
+                 uint64_t add2, uint64_t add3) {
+  uint64_t add[3] = {add1, add2, add3};
+  for (uint16_t i = 0; i < 3; ++i) {
+    if (value->bits[index] > ULLONG_MAX - add[i]) {
+      value->bits[index + 1] += MAX4BIT + 1L;
+      value->bits[index] -= ULLONG_MAX - add[i] + 1;
+    } else {
+      value->bits[index] += add[i];
+    }
+  }
+}
+
+void twos_complement_bits(work_decimal *value) {
+  for (uint16_t i = 0; i < WORKBITS; ++i) {
+    value->bits[i] = ~value->bits[i];
+    value->bits[i] &= MAX4BIT;
+  }
+  addnum(value, 1);
+}
+
+work_decimal subbits(work_decimal value_1, work_decimal value_2) {
+  work_decimal res;
+  twos_complement_bits(&value_2);
+  res = addbits(value_1, value_2);
+  return res;
+}
+
+work_decimal shiftleft(work_decimal value, uint16_t shift) {
+  work_decimal res = initwork();
+  uint16_t numbits = shift / 32;
+  shift %= 32;
+  for (int16_t i = WORKBITS - 1; i >= numbits; --i)
+    res.bits[i] = value.bits[i - numbits];
+  for (int16_t i = WORKBITS - 1; i >= 0; --i) res.bits[i] <<= shift;
+  for (int16_t i = WORKBITS - 1; i > 0; --i)
+    res.bits[i] += getoverflow(res.bits[i - 1]);
+  for (int16_t i = WORKBITS - 1; i >= 0; --i)
+    res.bits[i] = bitwithoutover(res.bits[i]);
+  return res;
+}
+
+work_decimal divmain(work_decimal v_1, work_decimal v_2, work_decimal *res) {
+  res->exp = 0;
+  for (int16_t i = 2; i >= 0; --i) {
+    for (int16_t j = 31; j >= 0; --j) {
+      work_decimal foo = shiftleft(v_2, i * 32 + j);
+      int compear = compearbits(v_1, foo);
+      if (compear >= 0) {
+        res->bits[i] += (uint64_t)1 << j;
+        v_1 = subbits(v_1, foo);
+        if (compear == 0) {
+          j = -1;
+          i = -1;
+        }
+      }
+    }
+  }
+  return v_1;
+}
+
+work_decimal divremain(work_decimal v_1, work_decimal v_2) {
+  for (int16_t i = 2; i >= 0; --i) {
+    for (int16_t j = 31; j >= 0; --j) {
+      work_decimal foo = shiftleft(v_2, i * 32 + j);
+      int compear = compearbits(v_1, foo);
+      if (compear >= 0) {
+        v_1 = subbits(v_1, foo);
+        if (compear == 0) {
+          j = -1;
+          i = -1;
+        }
+      }
+    }
+  }
+  return v_1;
+}
+
+void divtail(work_decimal v_1, work_decimal v_2, work_decimal *res) {
+  unsigned int stop = 0;
+  for (uint16_t i = 0; i < MAXEXP + 1 && stop == 0; ++i) {
+    stop = bits10up(res);
+    if (stop == 0) {
+      res->exp += 1;
+      bits10up(&v_1);
+      uint16_t add = 0;
+      for (int16_t j = 3; j >= 0; --j) {
+        work_decimal foo = shiftleft(v_2, j);
+        int compear = compearbits(v_1, foo);
+        if (compear >= 0) {
+          add += (uint64_t)1 << j;
+          v_1 = subbits(v_1, foo);
+          if (compear == 0) {
+            j = -1;
+            stop = 1;
+          }
+        }
+      }
+      addnum(res, add);
+    }
+  }
+}
+
+int char2num(char c) { return c - '0'; }
+
+s21_decimal set21(int bits3, int bits2, int bits1, int bits0) {
+  s21_decimal value;
+  value.bits[0] = bits0 & MAX4BIT;
+  value.bits[1] = bits1 & MAX4BIT;
+  value.bits[2] = bits2 & MAX4BIT;
+  value.bits[3] = bits3 & MAX4BIT;
+  return value;
+}
+
+work_decimal initwork() {
+  work_decimal value;
+  value.exp = 0;
+  for (uint16_t i = 0; i < WORKBITS; ++i) value.bits[i] = 0;
+  return value;
+}
