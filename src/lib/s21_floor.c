0a7617e594589286dd56061555a238e99f2f9fca
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-03-10 22:22:47 +0700

    project copied

---
+++ src/lib/s21_floor.c
@@ -0,0 +1,18 @@
+#include "s21_utils.h"
+
+int s21_floor(s21_decimal value, s21_decimal *result) {
+  int error = (result) ? 0 : 1;
+  if (iszero(value) && error == 0)
+    *result = set21(0, 0, 0, 0);
+  else if (!error) {
+    int sign = isminus(value), notint = 0;
+    work_decimal work_value = convert2work(value);
+    while (work_value.exp) {
+      int remainder = dellast(&work_value);
+      notint = (notint) ? notint : remainder;
+    }
+    if (notint && sign) addnum(&work_value, 1);
+    *result = convert2s21(work_value, sign);
+  }
+  return error;
+}
